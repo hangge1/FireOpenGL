0c4695c26011d14b132c48476616265b768c8091
HDD <824362330@qq.com>
2025-09-20 14:01:07 +0800

    1. submodule集成glfw和glad 2. 提供基本窗口初始化和退出的代码 3. build.ps1脚本快速cmake的配置和构建

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FireOpenGL
 This project record the progress for studying opengl 
+
+
+# Dependency
+1. python （我的是3.13.7）
+   - Module: jinja2
+2. glad
+3. glfw
+
---
+++ main.cpp
@@ -0,0 +1,88 @@
+
+#include <iostream>
+
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main(int argc, char* argv[])
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+#ifdef GLAD_OPTION_GL_MX
+	GladGLContext context = {};
+#ifdef GLAD_OPTION_GL_LOADER
+	int version = gladLoaderLoadGLContext(&context);
+#else
+	int version = gladLoadGLContext(&context, glfwGetProcAddress);
+#endif
+#else
+#ifdef GLAD_OPTION_GL_LOADER
+	int version = gladLoaderLoadGL();
+#else
+	int version = gladLoadGL(glfwGetProcAddress);
+#endif
+#endif
+
+	if (version == 0)
+	{
+		std::cout << "Failed to initialize OpenGL context" << std::endl;
+		return -1;
+	}
+	std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "." << GLAD_VERSION_MINOR(version) << std::endl;
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
